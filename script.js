7f51143182f760d4c12b41a6362e42c11873a528
zillur-rgb <zillurdeu@gmail.com>
2021-10-13 12:20:12 +0200

    Version 1.02

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let cScore = 0;
 let userChoice;
 let computerChoice;
 
-
 imgs.forEach((img) => {
   img.addEventListener("click", () => {
     userChoice = img.alt;
@@ -26,6 +25,7 @@ function calculate() {
       document.querySelector(
         ".uScore"
       ).innerHTML = `<h3 class="cScore">User Scores : ${uScore}</h3>`;
+      document.querySelector('.result').innerHTML = 'You won the turn!'
     } else if (
       (userChoice == "Paper" && computerChoice == 1) ||
       (userChoice == "Rock" && computerChoice == 2) ||
@@ -35,36 +35,40 @@ function calculate() {
       document.querySelector(
         ".cScore"
       ).innerHTML = `<h3 class="cScore">Computer Scores : ${cScore}</h3>`;
+      document.querySelector('.result').innerHTML = 'Computer won the turn!'
     } else {
-      alert("The game is tied!!");
+        document.querySelector('.result').innerHTML = 'The turn is tie'
     }
   }
   if (cScore == 5 || uScore == 5) {
-    document.querySelector('.popup').classList.add('active')
-    if(cScore == 5){
-        document.querySelector('.popup h3').innerHTML = `Computer has won the game by ${cScore} - ${uScore}!!!`
-        document.querySelector(
-            ".uScore"
-          ).innerHTML = `<h3 class="cScore">User Scores : ${uScore = 0}</h3>`;
-    
-          document.querySelector(
-            ".cScore"
-          ).innerHTML = `<h3 class="cScore">Computer Scores : ${cScore = 0}</h3>`;
-    } else{
-        document.querySelector('.popup h3').innerHTML = `User has won the game by ${uScore} - ${cScore}!!!`
-        document.querySelector(
-            ".uScore"
-          ).innerHTML = `<h3 class="cScore">User Scores : ${uScore = 0}</h3>`;
-    
-          document.querySelector(
-            ".cScore"
-          ).innerHTML = `<h3 class="cScore">Computer Scores : ${cScore = 0}</h3>`;
+    document.querySelector(".popup").classList.add("active");
+    if (cScore == 5) {
+      document.querySelector(
+        ".popup h3"
+      ).innerHTML = `Computer has won the game by ${cScore} - ${uScore}!!!`;
+      document.querySelector(
+        ".uScore"
+      ).innerHTML = `<h3 class="cScore">User Scores : ${(uScore = 0)}</h3>`;
+
+      document.querySelector(
+        ".cScore"
+      ).innerHTML = `<h3 class="cScore">Computer Scores : ${(cScore = 0)}</h3>`;
+    } else {
+      document.querySelector(
+        ".popup h3"
+      ).innerHTML = `User has won the game by ${uScore} - ${cScore}!!!`;
+      document.querySelector(
+        ".uScore"
+      ).innerHTML = `<h3 class="cScore">User Scores : ${(uScore = 0)}</h3>`;
+
+      document.querySelector(
+        ".cScore"
+      ).innerHTML = `<h3 class="cScore">Computer Scores : ${(cScore = 0)}</h3>`;
     }
   }
-
-
 }
 
-document.querySelector('.popup button').addEventListener('click', ()=>{
-    document.querySelector('.popup').classList.remove('active')
-})
+document.querySelector(".popup button").addEventListener("click", () => {
+  document.querySelector(".popup").classList.remove("active");
+  document.querySelector('.result').innerHTML = `Let's start again`;
+});
